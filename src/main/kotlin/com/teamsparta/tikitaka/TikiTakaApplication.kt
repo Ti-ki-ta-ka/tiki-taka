bf053f659e4a744bc434a81d802c69b38674dabe
임성우 <seongwoorang@naver.com>
2024-07-16 10:36:39 +0900

    init setting

---
+++ src/main/kotlin/com/teamsparta/tikitaka/TikiTakaApplication.kt
@@ -0,0 +1,11 @@
+package com.teamsparta.tikitaka
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TikiTakaApplication
+
+fun main(args: Array<String>) {
+    runApplication<TikiTakaApplication>(*args)
+}
---
+++ src/test/kotlin/com/teamsparta/tikitaka/TikiTakaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.teamsparta.tikitaka
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TikiTakaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
