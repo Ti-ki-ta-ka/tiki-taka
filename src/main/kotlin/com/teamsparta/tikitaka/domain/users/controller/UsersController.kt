90962fc4876d1cb4c6ce8e303394c1e7a9b71232
Jiho <https://github.com/HJH1111>
2024-07-16 14:37:51 +0900

    :sparkles: JWT, Security, 로그인 구현

--- src/main/kotlin/com/teamsparta/tikitaka/domain/users/controller/UsersController.kt
+++ src/main/kotlin/com/teamsparta/tikitaka/domain/users/controller/UsersController.kt
@@ -1,4 +1,24 @@
 package com.teamsparta.tikitaka.domain.users.controller
 
-class UsersController {
+import com.teamsparta.tikitaka.domain.users.dto.LoginRequest
+import com.teamsparta.tikitaka.domain.users.dto.LoginResponse
+import com.teamsparta.tikitaka.domain.users.service.v1.UsersService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/users")
+class UsersController(
+    private val usersService: UsersService
+)
+{
+    @PostMapping("/log_in")
+    fun logIn(
+        @RequestBody request: LoginRequest
+    ): ResponseEntity<LoginResponse> {
+        return ResponseEntity.ok(usersService.logIn(request))
+    }
 }
