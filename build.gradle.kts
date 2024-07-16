64d6d4815cc1223b987c05dc6c5df5f25bf9a74e
임성우 <seongwoorang@naver.com>
2024-07-16 11:11:07 +0900

    swagger 및 gradle 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,11 @@ plugins {
     id("io.spring.dependency-management") version "1.1.5"
     kotlin("jvm") version "1.9.24"
     kotlin("plugin.spring") version "1.9.24"
+    kotlin("plugin.noarg") version "1.9.24"
+    kotlin("plugin.allopen") version "1.9.24"
 }
 
-group = "com.teamsparta"
+group = "org.teamsparta"
 version = "0.0.1-SNAPSHOT"
 
 java {
@@ -13,6 +15,18 @@ java {
         languageVersion = JavaLanguageVersion.of(17)
     }
 }
+
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
+}
+noArg {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
+}
+
 
 configurations {
     compileOnly {
@@ -28,10 +42,28 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-aop")
+    implementation ("org.springframework.boot:spring-boot-starter")
+    implementation("io.jsonwebtoken:jjwt-api:0.12.3")
+//    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("com.amazonaws:aws-java-sdk-s3:1.12.741")
+    implementation("org.springframework.cloud:spring-cloud-starter-aws:2.2.6.RELEASE")
+    implementation ("org.springframework.boot:spring-boot-starter-data-redis")
+
+
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+
+    runtimeOnly("org.postgresql:postgresql")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.3")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.3")
+
 }
 
 kotlin {
