143efc9d5916d32bf22d9495fd45f9a48d93908f
taehuiii <swanna18@naver.com>
2024-07-16 20:56:36 +0900

    :sparkles: #7-feat-Match validation

--- src/main/kotlin/com/teamsparta/tikitaka/domain/matching/model/Match.kt
+++ src/main/kotlin/com/teamsparta/tikitaka/domain/matching/model/Match.kt
@@ -21,7 +21,7 @@ class Match(
     var content: String,
 
     @Column(name = "match_status", nullable = false)
-    var matchStatus: Boolean,
+    var matchStatus: Boolean = false,
 
     @Column(name = "post_team_id", nullable = false)
     var teamId: Long
@@ -32,6 +32,8 @@ class Match(
     var id: Long? = null
 
     fun updateMatch( request : UpdateMatchRequest){
+        validateTitle(request.title)
+        validateMatchDate(request.matchDate)
         this.title=request.title
         this.matchDate =request.matchDate
         this.location = request.location
@@ -41,15 +43,39 @@ class Match(
 
 
     companion object{
-        fun of(matchDate: LocalDateTime, location: String, content: String, matchStatus: Boolean, teamId: Long, title:String):Match{
+
+        fun of(matchDate: LocalDateTime, location: String, content: String, matchStatus: Boolean, teamId: Long, title: String): Match {
             return Match(
                 matchDate = matchDate,
                 location = location,
                 content = content,
                 matchStatus = matchStatus,
                 teamId = teamId,
-                title =title,
-            )
+                title = title,
+            ).apply {
+                validateTitle(title)
+                validateMatchDate(matchDate)
+                validateContent(content)
+            }
         }
     }
+
+    private fun validateTitle(title:String){
+        if(title.length >20){
+            throw RuntimeException("제목이 너무 깁니다.") //todo : custom exception
+        }
+    }
+
+    private fun validateMatchDate(matchDate: LocalDateTime) {
+        if (matchDate.isBefore(this.createdAt)) {
+            throw RuntimeException("매치 날짜는 생성 날짜 이후여야 합니다") // todo: custom exception
+        }
+    }
+
+    private fun validateContent(content:String){
+        if(content.length >1000){
+            throw RuntimeException("내용이 너무 깁니다.") //todo : custom exception
+        }
+    }
+
 }
