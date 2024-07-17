73477117eca23446b44c86a7107c2dbf4db6d06a
임성우 <seongwoorang@naver.com>
2024-07-17 15:54:08 +0900

    :sparkles: #15 - conflict 처리

--- src/main/kotlin/com/teamsparta/tikitaka/domain/users/service/v1/UsersServiceImpl.kt
+++ src/main/kotlin/com/teamsparta/tikitaka/domain/users/service/v1/UsersServiceImpl.kt
@@ -2,13 +2,14 @@ package com.teamsparta.tikitaka.domain.users.service.v1
 
 import com.teamsparta.tikitaka.domain.common.exception.InvalidCredentialException
 import com.teamsparta.tikitaka.domain.common.util.RedisUtils
-import com.teamsparta.tikitaka.domain.users.dto.LoginRequest
-import com.teamsparta.tikitaka.domain.users.dto.LoginResponse
-import com.teamsparta.tikitaka.domain.users.dto.SignUpRequest
-import com.teamsparta.tikitaka.domain.users.dto.UserDto
+import com.teamsparta.tikitaka.domain.users.dto.*
 import com.teamsparta.tikitaka.domain.users.model.Users
+import com.teamsparta.tikitaka.domain.users.model.toUpdateNameResponse
+import com.teamsparta.tikitaka.domain.users.model.toUpdatePasswordResponse
 import com.teamsparta.tikitaka.domain.users.repository.UsersRepository
+import com.teamsparta.tikitaka.infra.security.UserPrincipal
 import com.teamsparta.tikitaka.infra.security.jwt.JwtPlugin
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -40,7 +41,10 @@ class UsersServiceImpl(
     }
 
     override fun logIn(request: LoginRequest): LoginResponse {
-        val user = usersRepository.findByEmail(request.email) ?: throw RuntimeException("임시")
+        val user = usersRepository.findByEmail(request.email) ?: throw RuntimeException("email이 없습니다")
+        if (!passwordEncoder.matches(request.password, user.password)) {
+            throw RuntimeException("비밀번호가 맞지 않습니다")
+        }
         val accessToken = jwtPlugin.generateAccessToken(
             subject = user.id.toString(),
             email = user.email
@@ -67,6 +71,34 @@ class UsersServiceImpl(
             saveRefreshToken(newTokenInfo.refreshToken)
         }
         return newTokenInfo
+    }
+
+    override fun updateName(request: NameRequest, userPrincipal: UserPrincipal): NameResponse {
+        val user = usersRepository.findByIdOrNull(userPrincipal.id) ?: throw RuntimeException("임시")
+        if (user.id != userPrincipal.id) {
+            throw RuntimeException("인증되지 않은 사용자")
+        }
+        if (usersRepository.existsByName(request.name)) {
+            throw RuntimeException("이미 사용하고 있는 이름")
+        }
+
+        user.name = request.name
+        usersRepository.save(user)
+        return user.toUpdateNameResponse()
+    }
+
+    override fun updatePassword(request: PasswordRequest, userPrincipal: UserPrincipal): PasswordResponse {
+        val user = usersRepository.findByIdOrNull(userPrincipal.id) ?: throw RuntimeException("임시")
+        if (user.id != userPrincipal.id) {
+            throw RuntimeException("인증되지 않은 사용자")
+        }
+        if (usersRepository.existsByPassword(request.password)) {
+            throw RuntimeException("이미 사용하고 있는 패스워드")
+        }
+
+        user.password = passwordEncoder.encode(request.password)
+        usersRepository.save(user)
+        return user.toUpdatePasswordResponse()
     }
 
     fun isTokenBlacklisted(token: String): Boolean {
