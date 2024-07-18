b594b70daf6e91adb104ada02084407e52c37013
jinkownhong <hjk0472@gmail.com>
2024-07-18 11:59:25 +0900

    :sparkles: #39 - TeamMember, TeamRole 생성

---
+++ src/main/kotlin/com/teamsparta/tikitaka/domain/team/model/teamMember/TeamRole.kt
@@ -0,0 +1,15 @@
+package com.teamsparta.tikitaka.domain.team.model.teamMember
+
+enum class TeamRole {
+    LEADER, SUB_LEADER, MEMBER;
+
+    companion object {
+        fun fromString(teamRole: String): TeamRole {
+            return try {
+                valueOf(teamRole.uppercase())
+            } catch (e: IllegalArgumentException) {
+                throw IllegalArgumentException("Invalid category : $teamRole")
+            }
+        }
+    }
+}
