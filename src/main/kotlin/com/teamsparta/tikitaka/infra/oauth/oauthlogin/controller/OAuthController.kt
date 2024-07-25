db6c29de6f5ccfa6bf336124335d05f0191aa056
Jiho <https://github.com/HJH1111>
2024-07-25 16:09:26 +0900

    :adhesive_bandage: 컨트롤러 경로 수정

--- src/main/kotlin/com/teamsparta/tikitaka/infra/oauth/oauthlogin/controller/OAuthController.kt
+++ src/main/kotlin/com/teamsparta/tikitaka/infra/oauth/oauthlogin/controller/OAuthController.kt
@@ -4,10 +4,12 @@ import com.teamsparta.tikitaka.infra.oauth.oauthlogin.service.OauthLoginService
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/api/v2")
 class OAuthController(
     private val oauthLoginService: OauthLoginService
 ) {
