3deaf5a7ef6d2643124cf31cf361050c4c7e2f24
jinkownhong <hjk0472@gmail.com>
2024-07-26 14:23:14 +0900

    :bug: #91 - SecurityConfig 접근 권한 변경

--- src/main/kotlin/com/teamsparta/tikitaka/infra/security/config/SecurityConfig.kt
+++ src/main/kotlin/com/teamsparta/tikitaka/infra/security/config/SecurityConfig.kt
@@ -37,6 +37,14 @@ class SecurityConfig(
                     "/api/v1/matches/searches",
                     "/api/v1/matches/by-deadline",
                     "/api/v1/matches/available",
+
+                    "/api/v2/users/log-in",
+                    "/api/v2/users/sign-up",
+                    "/api/v2/matches/**",
+                    "/api/v2/users/token/refresh",
+                    "/api/v2/matches/searches",
+                    "/api/v2/matches/by-deadline",
+                    "/api/v2/matches/available",
                 ).permitAll()
                     .anyRequest().authenticated()
             }
