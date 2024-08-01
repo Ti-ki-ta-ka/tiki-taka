99a60ac58e0681fc1a514bf94433c8e9b697ebd1
Jiho <https://github.com/HJH1111>
2024-08-01 20:43:57 +0900

    :sparkles: 평가 기능 구현

---
+++ src/main/kotlin/com/teamsparta/tikitaka/domain/evaluation/service/v3/EvaluationService.kt
@@ -0,0 +1,9 @@
+package com.teamsparta.tikitaka.domain.evaluation.service.v3
+
+import com.teamsparta.tikitaka.domain.evaluation.dto.EvaluationRequest
+import com.teamsparta.tikitaka.domain.evaluation.dto.EvaluationResponse
+import com.teamsparta.tikitaka.infra.security.UserPrincipal
+
+interface EvaluationService {
+    fun evaluate(evaluationId: Long, principal: UserPrincipal, request: EvaluationRequest): EvaluationResponse
+}
