7a27cce1f8ed2cc21b1fd71c6819da9a4c428868
임성우 <seongwoorang@naver.com>
2024-08-02 14:51:40 +0900

    ✨ #116 -  update evaluation using batch (#120)
    * 90일 내 점수 갱신 Batch 작업
    
    * Conflict 처리
    
    * :bug: #116 - 배치 활용 데이터 기간 변경

--- src/main/kotlin/com/teamsparta/tikitaka/domain/evaluation/repository/EvaluationRepository.kt
+++ src/main/kotlin/com/teamsparta/tikitaka/domain/evaluation/repository/EvaluationRepository.kt
@@ -3,4 +3,4 @@ package com.teamsparta.tikitaka.domain.evaluation.repository
 import com.teamsparta.tikitaka.domain.evaluation.model.Evaluation
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface EvaluationRepository : JpaRepository<Evaluation, Long>
+interface EvaluationRepository : JpaRepository<Evaluation, Long>, CustomEvaluationRepository
